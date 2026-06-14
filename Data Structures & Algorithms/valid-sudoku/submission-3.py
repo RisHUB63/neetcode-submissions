9774aaa416c15043682792afb7468add66ab920a
Rishabh Sharma <91869180+RisHUB63@users.noreply.github.com>
2026-06-14 20:23:11 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+
+    def box_id(self, row, col):
+        return (row // 3) * 3 + (col // 3)
+    
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        memory = {"box":{}, "col":{}, "row": {}}
+
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == '.':
+                    continue
+                
+                box_id = self.box_id(row, col)
+
+                if box_id not in memory["box"]:
+                    memory["box"][box_id] = []
+                if col not in memory["col"]:
+                    memory["col"][col] = []
+                if row not in memory["row"]:
+                    memory["row"][row] = []
+
+                if ((box_id in memory["box"] and board[row][col] in memory["box"][box_id]) 
+                or (col in memory["col"] and board[row][col] in memory["col"][col]) 
+                or (row in memory["row"] and board[row][col] in memory["row"][row])):
+                    return False
+
+
+                memory["box"][box_id].append(board[row][col])
+                memory["col"][col].append(board[row][col])
+                memory["row"][row].append(board[row][col])
+        
+        return True
+
+
+
+
+        
